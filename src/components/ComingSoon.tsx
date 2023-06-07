6e8e3c8f31d7e2fa8fe390db7bd8a2ef95694c68
allanviguilla <viguilla.allan@gmail.com>
2023-06-07 13:52:28 -0700

    Style changes

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -16,23 +16,13 @@ export default function ComingSoon() {
     <VStack bg="primary">
       <Flex h="100vh" direction="column" align="center">
         <Box m="auto">
-          <Box position="relative" bottom="-50" left="-100" zIndex="overlay">
+          <Box>
             <Heading textAlign="center" color="white" size="3xl">
               BOOK CLUB
             </Heading>
             <Text textAlign="center" color="white" fontSize="2xl">
               COMING SOON <br /> SUMMER 2025
             </Text>
-          </Box>
-          <Box>
-            <Center>
-              <Image
-                borderRadius="20"
-                boxSize="275"
-                src="https://imagizer.imageshack.com/img922/8487/fKsV55.jpg"
-                alt="Book Club Teaser"
-              />
-            </Center>
           </Box>
         </Box>
       </Flex>
